435975e09748101e583f2467d9762887b34abf8c
devyosse <narutoshippuden884@gmail.com>
2021-11-16 13:39:11 +0100

    exo 65

---
+++ app.js
@@ -0,0 +1,16 @@
+let elements = document.getElementsByClassName('paragraphe');
+
+for(let i = 0; i < elements.length; i++){
+
+    if((i + 1) % 2 === 0){
+        continue;
+    }
+
+    else if (i === elements.length){
+        break;
+    }
+
+    else{
+        elements[i].innerHTML = i + 1;
+    }
+}
